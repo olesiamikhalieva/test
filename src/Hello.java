7cbd9a2c33816cf57cea2d2e3a6aaaf29fc3ea50
Olesia Mikhalieva <lesenok808@gmail.com>
2017-12-25 20:40:58 +0200

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        int b=23;
+        int c=45;
+        System.out.println(b+c);
     }
 }
